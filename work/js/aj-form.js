352701634576d3501124651571de2ab22f285b26
peipage <654605047@qq.com>
2017-04-17 01:24:45 +0800

    增加验证密码是否一致的directive指令

--- work/js/aj-form.js
+++ work/js/aj-form.js
@@ -1,10 +1,28 @@
 angular.module('myApp', [])
-    .controller('MainController',function($scope)
+    //创建一个控制器controller
+    .controller('loginController',function($scope)
     {
         $scope.userData={};
-        $scope.submitForm=function()
+        $scope.loginForm=function()
         {
             console.log($scope.userData);
 
         }
+    })
+    .directive('compare',function () {
+       var o ={};
+       o.strict='AE';
+       o.scope={
+           orgText:'=compare'
+       };
+       o.require='ngModel';
+       o.link=function(sco,ele,att,con){
+           con.$validators.compare=function(v){
+               return v == sco.orgText;
+           };
+           sco.$watch('orgText',function () {
+               con.$validate();
+           })
+        };
+        return o;
     });
