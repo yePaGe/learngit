1fd90eaeb3f70f11962362dc1d56db537c7b60c0
peipage <654605047@qq.com>
2017-04-19 20:31:59 +0800

    增加了顶部nav的鼠标事件，为<li>标签里面添加<a>标签，方便鼠标事件的实现“

--- work/js/jingdong.js
+++ work/js/jingdong.js
@@ -1,7 +1,45 @@
-angular.module('myApp',[])
-    .controller('MainController',function ($scope) {
-        $scope.userData={};
-        $scope.submitForm = function () {
-            console.log($scope.userData);
+
+    $(".mess").on({
+        mouseover:function () {
+            $(this).addClass("active");
+            $(this).children("div").show();
+        },
+        mouseout:function () {
+        $(this).removeClass("active");
+            $(this).children("div").hide();
+
         }
     });
+    $(".locationMess>ul>li>a").on({
+        mouseover:function () {
+            $(this).css("background-color","rgba(227,228,229,1)");
+        },
+        mouseout:function () {
+            $(this).css("background-color","rgba(255,255,255,1)") ;
+        },
+        click:function () {
+            var lName = $(this).text();
+            $(".lo").text(lName);
+            $(this).parents(".locationMess").hide();
+        }
+    });
+    $(".location").mouseover(function () {
+        $(".locationMess").show();
+        $(this).addClass("locationHover");
+       /* $(".locationMess>ul>li>a").forEach(function () {
+           var name = $(this).text();
+           if(name==$(".lo").text()){
+               $(this).addClass("messChoose");
+           }
+        });*/
+    });
+    $(".location").mouseout(function () {
+        $(this).removeClass("locationHover");
+        $(".locationMess").hide();
+    });
+
+
+
+
+
+
